819621b704cb7e9600d1032e2a5f62a8dcd6819c
ola-dola <oredolaolamide@gmail.com>
2020-01-13 15:55:24 +0100

    wip: api serving list of users

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 // implement your API here
 const express = require('express');
 
+const { find, findById, insert, update, remove } = require('./data/db');
+
 const app = express();
 
 app.use(express.json());
 
+//root?
 app.get('/', (req, res) => {
-    res.json('We\'re live and rocking it (o_o)');
+  res.json('We\'re live and rocking it (o_o)');
+});
+
+//Get all users
+app.get('/api/users', (req, res) => {
+  find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ 
+        // message: error.message
+        errorMessage: "The users information could not be retrieved." 
+      })
+    })
+})
+
+//Get specific user by id
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+ 
+})
+
+//Add new user to db
+app.post('/api/users', (req, res) => {
+
+})
+
+//Modify user details
+app.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+})
+
+app.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
 })
 
 app.listen(5100, () => {
-    console.log('listening on port 5100');
+  console.log('listening on port 5100');
 });
