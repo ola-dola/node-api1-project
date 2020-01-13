6f53a16167eff9005d97a3f0009b0ec933a45837
ola-dola <oredolaolamide@gmail.com>
2020-01-13 15:34:06 +0100

    wip: dummy server live

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('We\'re live and rocking it (o_o)');
+})
+
+app.listen(5100, () => {
+    console.log('listening on port 5100');
+});
